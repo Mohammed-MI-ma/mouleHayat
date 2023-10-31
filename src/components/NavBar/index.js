5d16a83de97865ab9b2634865676d50c49f4ddd6
Mohammed Aboutalha <mohammedaboutalha2023@gmail.com>
2023-10-31 20:55:24 +0000

    Animation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,35 +8,37 @@ import logoImage from "../assets/png/logo.png"; // Replace with the actual path 
 const NavBar = () => {
   return (
     <>
-    <div className={`container ${style.logoContainer}`}>
-      <div className={style.logoText}>
-        <div style={{ position: "relative" }}>
-          <Image
-            src={logoImage}
-            alt="Logo"
-            preview={false}
-            style={{ width: "120px" }}
-          />
-          <div
-            style={{
-              borderRadius: " 50%",
-              background: "white",
-              width: "80px",
-              height: "80px",
-              position: "absolute",
-              top: " 0",
-              left: "0",
-              transform: " translate(20px, 20px)",
-              zIndex: "-1",
-            }}
-          ></div>
+      <div className={`container ${style.logoContainer}`}>
+        <div className={style.logoText}>
+          <div style={{ position: "relative" }}>
+            <Image
+              src={logoImage}
+              alt="Logo"
+              preview={false}
+              style={{ width: "120px" }}
+            />
+            <div
+              style={{
+                borderRadius: " 50%",
+                background: "white",
+                width: "80px",
+                height: "80px",
+                position: "absolute",
+                top: " 0",
+                left: "0",
+                transform: " translate(20px, 20px)",
+                zIndex: "-1",
+              }}
+            ></div>
+          </div>
+          HayatMoul
         </div>
-      </div>
 
-      <Button className={style.languageIcon}>
-        <DropDownLanguages />
-      </Button>
-    </div></>
+        <Button className={style.languageIcon}>
+          <DropDownLanguages />
+        </Button>
+      </div>
+    </>
   );
 };
 
